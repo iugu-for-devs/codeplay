834f6f6da99c3f20a5aebcb531c7650297c71a23
DanTupi <danilotupinamba@hotmail.com>
2021-04-14 21:27:55 -0300

    corrigindo rubocop

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -4,26 +4,26 @@ RSpec.describe Admin, type: :model do
   it 'admin can log with valid email and password' do
     admin = Fabricate(:admin)
 
-    login_as(admin, :scope => :admin)
+    login_as(admin)
 
     expect(admin.valid_password?(admin.password)).to be_truthy
     expect(admin.email).to eq('jane.doe@codeplay.com.br')
   end
-  
+
   it 'admin can not log with valid email but unvalid password' do
     admin = Fabricate(:admin)
 
-    login_as(admin, :scope => :admin)
+    login_as(admin)
 
     expect(admin.valid_password?('1234567')).to be_falsey
-  end 
+  end
 
   it 'admin can not log with invalid email and unvalid password' do
     admin = Fabricate(:admin, email: 'teste@codeplay.com.br')
 
-    login_as(admin, :scope => :admin)
+    login_as(admin)
 
     expect(admin.valid_password?('1234567')).to be_falsey
     expect(admin.email).not_to eq('jane.doe@codeplay.com.br')
-  end 
+  end
 end
