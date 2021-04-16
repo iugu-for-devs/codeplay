cb3aa6b6ced1dfb550aceb60f1af034a3a1d95f9
lBenevides <lhenriquebenevides@gmail.com>
2021-04-16 19:48:54 -0300

    Adicionado index unique para coluna name, tabela subscription

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_202445) do
+ActiveRecord::Schema.define(version: 2021_04_16_224507) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_04_14_202445) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "price", null: false
+    t.index ["name"], name: "index_subscriptions_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
