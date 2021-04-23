78af178af7e13c92dbfb537bbce2398bfc103961
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-04-22 23:06:09 -0300

    Implementação de PORO para simular a geração de faturas pela plataforma de pagamento
    Co-authored-by: MarcosAntonio<marcosantoniotaina@gmail.com>

---
+++ app/models/generate_invoice.rb
@@ -0,0 +1,18 @@
+class GenerateInvoice
+	
+	def initialize(token_course:,token_user:, token_pay_type:)
+		@token_course = ''
+		@token_user = ''
+		@token_pay_type = '' 
+	end
+
+	def generated_invoice
+    possible_status = ['refused', 'pending', 'approved']
+		returned_token = '0123456789'
+
+		{ status: possible_status.sample , token: returned_token }
+	end
+end
+
+# TODO: Devemos persistir pedido de faturas recusados? acho que sim
+# TODO: Devemos ter token de pedido de faturas recusada ? acho que sim
