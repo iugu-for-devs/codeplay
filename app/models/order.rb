f97b4b60936ddb974b84c0f3b1606672bf553c97
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-04-22 23:11:51 -0300

    Implementando método de instância de Order para enviar e obter o retorno da plataforma de pagamentor assim setar o status de pedido e o token do pedido. TODO: nas regras de negócio passadas api da plataforma de pagamentos não envia um token pelo json. Co-authored-by: MarcosAntonio<marcosantoniotaina@gmail.com>

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,10 @@
 class Order < ApplicationRecord
   validates :pay_type, presence: true
+
+  def send_invoice_request 
+  	GenerateInvoice.new(course_token: course.token, 
+  		                  user_token: user.token, 
+  		                  pay_type_token: pay_type )
+  								 .generate_invoice
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,79 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:pay_type) } 
+  it { should validate_presence_of(:status) }
+  it { should validate_presence_of(:token) }
+
+	it{ shoul belong_to :course }
+	it{ should belong_to :user }
+
+  # método de intancia já que iremos esperar valores que modifiquem o status do produto
+
+  context 'when create a order' do
+		context '.send_invoice_request' do	 
+			
+			it 'can change order status' do
+
+			  client = Fabricate(:user)
+			  course = Fabricate(:course)
+			  payment_method_chosen = PayType.all.first
+
+			  order = Order.new(user: client, course: course, pay_type: payment_method_chosen )
+			  old_order_status = 'pending'
+			  returned_token = Faker::Alphanumeric.alphanumeric(number: 10)
+
+			  allow(order).to receive(:send_invoice_request).and_return({ status: "approved",
+			  																													  token: returned_token
+			  																												  })
+
+			  expect(order.status).to_not eq(old_order_status)
+			end
+
+			it 'must set order token' do
+
+			  client = Fabricate(:user)
+			  course = Fabricate(:course)
+			  payment_method_chosen = PayType.all.first
+			  order = Order.new(user: client, course: course, pay_type: payment_method_chosen )
+			  returned_token = Faker::Alphanumeric.alphanumeric(number: 10)
+
+			  allow(order).to receive(:send_invoice_request).and_return({ status: "approved",
+			  																													  token: returned_token
+			  																												  })
+
+			  expect(order.token).to eq(returned_token)
+			end
+
+			it 'can changing the status to approved' do
+				
+				client = Fabricate(:user)
+			  course = Fabricate(:course)
+			  payment_method_chosen = PayType.all.first
+			  order = Order.new(user: client, course: course, pay_type: payment_method_chosen )
+			  returned_token = Faker::Alphanumeric.alphanumeric(number: 10)
+
+			  allow(order).to receive(:send_invoice_request).and_return({ status: "approved",
+			  																													  token: returned_token
+			  																												  })
+
+			  expect(order.status).to eq('approved')
+			end
+
+			it 'can changing the status to refused' do
+
+				client = Fabricate(:user)
+			  course = Fabricate(:course)
+			  payment_method_chosen = PayType.all.first
+			  order = Order.new(user: client, course: course, pay_type: payment_method_chosen )			  
+			  returned_token = Faker::Alphanumeric.alphanumeric(number: 10)
+
+			  allow(order).to receive(:send_invoice_request).and_return({ status: "refused",
+			  																													  token: returned_token
+			  																												  })
+
+			  expect(order.status).to eq('refused')				
+			end
+		end
+	end
 end
