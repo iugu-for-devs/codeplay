0fc7328fb80c234d0c20961a83c99ee3a7c39e64
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-04-24 16:22:21 -0300

    Implementação de testes de header da home e criação de uma partil para uso desse header.

--- spec/system/home_page_spec.rb
+++ spec/system/home_page_spec.rb
@@ -7,21 +7,29 @@ describe 'Home page' do
 			it 'can see link buttons' do
 				visit root_path
 
-				within '.header' do
-					expect(page).to have_link('Home', href: root_path)
-					expect(page).to have_link('Cursos', href: courses_path)
-					expect(page).to have_link('Assinaturas', href: subscriptions_path)
-					expect(page).to have_link('Entrar', href: new_user_session)
-					expect(page).to have_link('Nova Conta', href: new_user_registration)
-				end
+				expect(page).to have_link('Home', href: root_path)
+				expect(page).to have_link('Cursos', href: courses_path)
+				expect(page).to have_link('Assinaturas', href: subscriptions_path)
 			end
 
 			it 'can see company logo' do
 				visit root_path
 
-				within '.logo' do 
-					expect(page).to have_css("img[src*='logo-code-play.jpg']")
-				end
+				expect(page).to have_css("img[src*='logo-codeplay-black.svg']")
+			end
+
+			it 'can see login button' do 
+				visit root_path
+
+				click_on 'Entrar'
+				expect(current_path).to eq(new_user_session_path)
+			end
+
+			it 'can see sign up button' do
+				visit root_path
+
+				click_on 'Nova Conta'
+				expect(current_path).to eq(new_user_registration_path)
 			end
 		end
 	end
