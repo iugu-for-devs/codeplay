39a9f6fc65a6e6077ea50ebf05ff0ccbb147d125
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-04-25 22:13:02 -0300

    WIP Adicionando faker ao fabricator subscription.

--- spec/fabricators/course_fabricator.rb
+++ spec/fabricators/course_fabricator.rb
@@ -1,5 +1,5 @@
 Fabricator(:course) do
-  name {"Curso de #{ Faker::ProgrammingLanguage.unique.name }" }
+  name { "Curso de #{Faker::ProgrammingLanguage.unique.name}" }
   description { Faker::Lorem.paragraphs(number: 1) }
   price { Faker::Commerce.price(range: 50.00..400.00) }
   admin
