8a67613b7c007adb8abca98d5e3f55cd64f08dc4
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-04-26 10:43:05 -0300

    Adicionando rake task para geração de cursos, admins, assinaturas e alunos.

--- spec/fabricators/course_fabricator.rb
+++ spec/fabricators/course_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:course) do
   name { "Curso de #{Faker::ProgrammingLanguage.unique.name}" }
-  description { Faker::Lorem.paragraphs(number: 1) }
+  description { Faker::Lorem.paragraph }
   price { Faker::Commerce.price(range: 50.00..400.00) }
   admin
 end
