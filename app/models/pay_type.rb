229d0389a444820c16614dc7c3b46c4faac112e9
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-04-27 20:51:12 -0300

    Utilizando faraday para requisitar os tipos de pagamento
    Co-authored-by: danilorpsilva<pro.danilorps@gmail.com>

--- app/models/pay_type.rb
+++ app/models/pay_type.rb
@@ -7,12 +7,13 @@ class PayType
   end
 
   def self.all
-    [
-      new(name: 'Boleto', token: '0123456789'),
-      new(name: 'Cartão de Crédito', token: '3333333333'),
-      new(name: 'PIX', token: '4444444444')
-    ]
+    url = "https://my-json-server.typicode.com/JorgeLAB/codeplay/paytypes"
+    response = Faraday.get(url, 'Content-Type' => 'application/json')
+    pay_types = JSON.parse(response.body, symbolize_names: true)
+    pay_types.map { |pay_type| new(name: pay_type[:name], token: pay_type[:token] ) }
   end
+
+  def load_url(endpoint, company_key); end
 end
 
 # TODO: Plataforma de pagamento deve retornar os tipos de pagamento
